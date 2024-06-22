ebef8fff2efab5a9cfb2a08760481b5de13f339c
Tomek Leks <tomek@el-jot.eu>
2024-06-22 07:08:14 +0200

    added basic schema, need to add many to many for user roles

--- src/schema.rs
+++ src/schema.rs
@@ -1,7 +1,40 @@
 // @generated automatically by Diesel CLI.
 diesel::table! {
     user (id) {
-        id -> Integer
+        id -> Integer,
+        username -> VarChar,
+        email -> VarChar,
+        password -> VarChar,
+        created_at -> Timestamp,
+        account_valid -> Bool,
     }
 }
 
+diesel::table! {
+    confirmation_token (id) {
+        id -> Integer,
+        user_id -> Integer,
+        token -> VarChar,
+        created_at -> Timestamp,
+        expires_at -> Timestamp,
+        confirmed_at -> Timestamp,
+    }
+}
+
+diesel::table! {
+    roles (id) {
+        id -> Int4,
+        name -> VarChar,
+    }
+}
+
+diesel::table! {
+    user_roles (user_id) {
+        user_id -> Integer,
+        role_id -> Int4
+    }
+}
+
+diesel::joinable!(confirmation_token -> user (user_id));
+
+diesel::allow_tables_to_appear_in_same_query!(user, confirmation_token);
