ed58a7df2a7727b42bc4b4ce55134c5a41088ad0
Tomek <lekstomek602@gmail.com>
2024-06-26 21:23:26 +0200

    Added models for other tables

--- src/models.rs
+++ src/models.rs
@@ -13,3 +13,31 @@ pub struct User {
     pub created_at: NaiveDateTime,
     pub account_valid: bool,
 }
+
+#[derive(Queryable, Debug, Serialize, Deserialize, Selectable, Insertable)]
+#[diesel(table_name = crate::schema::roles)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Role {
+    pub id: i32,
+    pub name: String,
+}
+
+#[derive(Queryable, Debug, Serialize, Deserialize, Selectable, Insertable)]
+#[diesel(table_name = crate::schema::user_roles)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct UserRole {
+    pub user_id: i32,
+    pub role_id: i32,
+}
+
+#[derive(Queryable, Debug, Serialize, Deserialize, Selectable, Insertable)]
+#[diesel(table_name = crate::schema::confirmation_tokens)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct ConfirmationToken {
+    pub id: i32,
+    pub user_email: String,
+    pub token: String,
+    pub created_at: NaiveDateTime,
+    pub expires_at: NaiveDateTime,
+    pub confirmed_at: Option<NaiveDateTime>,
+}
