93e9bead0709d4a41a72384fb1764e317edf644f
Tomek Leks <tomek@el-jot.eu>
2024-09-18 10:57:46 +0200

    No errors no more time for testing jwt functions

--- src/auth/jwt/mod.rs
+++ src/auth/jwt/mod.rs
@@ -3,4 +3,6 @@ mod decoding;
 mod generation;
 mod verify;
 pub use claims::Claims;
+pub use decoding::jwt_decode;
 pub use generation::generate;
+pub use verify::verify;
--- src/auth/jwt/verify.rs
+++ src/auth/jwt/verify.rs
@@ -1,18 +1,23 @@
+use crate::{constants::JWT_EXPIRATION_TIME, est_conn, DPool};
+use chrono::Utc;
+use diesel::RunQueryDsl;
+use diesel::{
+    query_dsl::methods::FilterDsl,
+    result::Error as DieselError,
+    ExpressionMethods,
+};
 use std::usize;
 
-use crate::{constants::JWT_EXPIRATION_TIME, est_conn, DPool};
-use diesel::{
-    query_dsl::methods::{FilterDsl, SelectDsl},
-    result::Error as DieselError,
-    ExpressionMethods, QueryDsl, RunQueryDsl,
-};
-
-fn verify_email(user_email: &str, pool: DPool) -> Result<bool, DieselError> {
+fn verify_email(users_email: String, pool: DPool) -> Result<bool, DieselError> {
     use crate::schema::users::dsl::*;
 
+    let exists = diesel::select(diesel::dsl::exists(users.filter(email.eq(users_email))))
+        .get_result(&mut est_conn(pool.clone()));
+
     match exists {
-        Some(_) => Ok(true),
-        None => Ok(false),
+        Ok(true) => Ok(true),
+        Ok(false) => Ok(false),
+        Err(e) => Err(e),
     }
 }
 
@@ -27,13 +32,13 @@ fn verify_date(iat: usize, exp: usize) -> bool {
 }
 
 pub fn verify(token: &str, pool: DPool) -> bool {
-    match super::decode(token.to_string()) {
+    match super::jwt_decode(token.to_string()) {
         Ok(token) => {
             if !verify_date(token.claims.iat, token.claims.exp) {
                 return false;
             }
 
-            match verify_email(&token.claims.email, pool) {
+            match verify_email(token.claims.email, pool) {
                 Ok(true) => true,
                 Ok(false) => {
                     eprintln!("Email not found");
