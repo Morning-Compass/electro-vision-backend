a07d90c73df2a8a1f21618900c45662cc35da88c
Tomek <lekstomek602@gmail.com>
2024-09-22 11:17:38 +0200

    wokring on tests for jwt

--- src/auth/jwt/test.rs
+++ src/auth/jwt/test.rs
@@ -2,16 +2,32 @@
 mod tests {
     use core::panic;
 
-    use crate::auth;
+    use jsonwebtoken::TokenData;
+
+    use crate::auth::{self, jwt::{jwt_decode, Claims}};
 
     const TEST_EMAIL: &str = "tomek@el-jot.eu";
 
     fn generate_token() -> String {
         match auth::jwt::generate(&TEST_EMAIL) {
             Ok(token) => {
-                print!("token: {}", token)
+                println!("token: {}", token);
+                token
             }
-            Err(e) => panic!("Error generating token"),
+            Err(e) => panic!("Error generating token, {}", e),
+        }
+    }
+
+    fn decode() -> Result<TokenData<Claims>, jsonwebtoken::errors::Error> {
+        match jwt_decode(generate_token()) {
+            Ok(claims) => {
+                println!("Decoded claims: {:?}", claims.claims);
+                Ok(claims)
+            },
+            Err(e) => {
+                println!("Failed to decode token: {:?}", e);
+                Err(e)
+            }
         }
     }
 }
