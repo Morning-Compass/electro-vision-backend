e81920e1203a95ce52e2ce373d84384503de2a47
Tomek <lekstomek602@gmail.com>
2024-09-22 12:01:17 +0200

    test user problems resolved

--- src/auth/login/test.rs
+++ src/auth/login/test.rs
@@ -3,9 +3,19 @@ mod tests {
     use chrono::Utc;
 
     pub use crate::auth::login::login::UserWithRoles;
-    pub use crate::models::{User, Role};
+    pub use crate::models::User;
 
     fn login_with_roles() {
-        const USER_DATA: UserWithRoles = UserWithRoles::new(User{id: 1, username: String::from("Oaza_spokoju"), email: String::from("oaza_spokoju@gmail.com"), password: String::from("oazaSpokoju123!"), created_at: Utc::now().naive_utc(), account_valid: false}, Vec::from([Role{id: 1, name: String::from("SpokojNasUratuje")}]));
+        let user_data: UserWithRoles = UserWithRoles::new(
+            User {
+                id: 1,
+                username: String::from("Oaza_spokoju"),
+                email: String::from("oaza_spokoju@gmail.com"),
+                password: String::from("oazaSpokoju123!"),
+                created_at: Utc::now().naive_utc(),
+                account_valid: false,
+            },
+            vec![String::from("Spokojna rola")],
+        );
     }
 }
