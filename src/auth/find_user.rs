d67caa14944f14516cc2bcf4b06d6a030e5a1b54
JaQubus <jakubkulik20@gmail.com>
2024-10-02 19:33:02 +0200

    Created and added methods to find user if the user exists and methods to get user data

--- src/auth/find_user.rs
+++ src/auth/find_user.rs
@@ -1,35 +1,50 @@
-use diesel::QueryDsl;
-use diesel::prelude::*;
-use diesel::{result::Error as DieselError, ExpressionMethods};
 use crate::models::User;
-use crate::user::list_users;
 use crate::{est_conn, schema, DPool};
+use diesel::prelude::*;
+use diesel::QueryDsl;
+use diesel::{result::Error as DieselError, ExpressionMethods};
 
 pub struct FindData {
     pub is_found: Result<bool, DieselError>,
 }
 
 pub trait Find {
-    async fn find_by_email(email: String, pool: DPool) -> Result<bool, DieselError>;
+    async fn exists_by_email(email: String, pool: DPool) -> Result<bool, DieselError>;
+    async fn find_by_email(email: String, pool: DPool) -> Result<User, DieselError>;
 }
 
 impl Find for FindData {
-    async fn find_by_email(_email: String, pool: DPool) -> Result<bool, DieselError> {
+    async fn exists_by_email(_email: String, pool: DPool) -> Result<bool, DieselError> {
         use schema::users::dsl::*;
         let conn = &mut est_conn(pool.clone());
-        let user_data = list_users(1, pool).await;
-        let is_found = diesel::select(diesel::dsl::exists(users.select(email).filter(email.eq(_email)))).get_result(conn);
-        println!("\n\nUser data? {:?} \n\n", user_data);
-        println!("\n\nWas found? {:?} \n\n", is_found);
+        let is_found = diesel::select(diesel::dsl::exists(
+            users.select(email).filter(email.eq(_email.clone())),
+        ))
+        .get_result::<bool>(conn);
         match is_found {
             Ok(true) => {
                 println!("User found.");
                 Ok(true)
-            },
+            }
             Ok(false) => {
                 println!("User not found.");
                 Ok(false)
-            },
+            }
+            Err(e) => Err(e),
+        }
+    }
+    async fn find_by_email(_email: String, pool: DPool) -> Result<User, DieselError> {
+        use schema::users::dsl::*;
+        let conn = &mut est_conn(pool.clone());
+        let user_data = users
+            .filter(email.eq(_email))
+            .select(User::as_select())
+            .first(conn);
+        match user_data {
+            Ok(user) => {
+                println!("User data was found find_user.rs {:?}", user);
+                Ok(user)
+            }
             Err(e) => Err(e),
         }
     }
