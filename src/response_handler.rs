46d0411022f2c99b9830ca9c7525df11ed8e2dca
Jakub <jakubkulik20@gmail.com>
2024-11-12 09:53:40 +0100

    Created json-handler class

---
+++ src/response_handler.rs
@@ -0,0 +1,37 @@
+use crate::File;
+use serde_json::Value;
+
+pub struct ResponseHandler {
+    pub json_data: String,
+}
+
+pub struct ResponseData {
+    pub resp_name: ResponseValues,
+}
+
+pub struct ResponseValues {
+    pub key: String,
+    pub message: String,
+    pub code: String,
+}
+
+pub trait Response {
+    async fn file_get_contents(file_name: String) -> Result<Value, String>;
+}
+
+impl Response for ResponseHandler {
+    async fn file_get_contents(file_name: String) -> Result<Value, String> {
+        // Try to open the file
+        let file = File::open(file_name);
+        let file = match file {
+            Ok(file) => file,
+            Err(e) => return Err(e.to_string()),
+        };
+
+        let data: Result<Value, serde_json::Error> = serde_json::from_reader(file);
+        match data {
+            Ok(json_data) => Ok(json_data),
+            Err(e) => Err(e.to_string()),
+        }
+    }
+}
